8a359066a710c999f5564575675da1602ef048ce
german283 <germanclarinet03@gmail.com>
2021-04-22 10:48:19 -0500

    no message

---
+++ Expoandes2/app/src/main/java/com/example/expoandes/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.expoandes.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
