e5dbe016bb40ad69a98ce43469bbaa96f07c842b
german283 <germanclarinet03@gmail.com>
2021-04-26 19:23:27 -0500

    Configuración básica de la app para conectarse con firebase

--- Expoandes/app/src/main/java/com/example/expoandes/ui/home/HomeFragment.kt
+++ Expoandes/app/src/main/java/com/example/expoandes/ui/home/HomeFragment.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.expoandes.R
+import com.google.firebase.firestore.FirebaseFirestore
 
 class HomeFragment : Fragment() {
+
+    private val db = FirebaseFirestore.getInstance()
 
     private lateinit var homeViewModel: HomeViewModel
 
@@ -22,10 +26,15 @@ class HomeFragment : Fragment() {
         homeViewModel =
                 ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
+        val boton_prueba=root.findViewById<Button>(R.id.boton_prueba)
+
+        boton_prueba.setOnClickListener(){
+            println("hola")
+            db.collection("Mingle_users").document("prueba").update(
+                hashMapOf("qiiii" to "hola") as Map<String, Any>
+            ) 
+        }
         return root
     }
 }
